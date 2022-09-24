7b573b7afef2aee45bbb0207c4884a3398b78984
LeonidBabkin <Babkinleo@gmail.com>
2022-09-24 15:45:21 +0300

    prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,9 +4,10 @@ TASK = 'Answer \"yes\" if given number is prime. Otherwise answer \"no\".'
 
 
 def calc_func():
-    question = randrange(1, 1000)
-    for i in range(2, question):
-        if(question % i) == 0:
+    num = randrange(1, 1000)
+    question = f'Question: {num}'
+    for i in range(2, num):
+        if (num % i) == 0:
             answer = 'no'
             return question, str(answer)
         answer = 'yes'
