88dfc20fe1c0486e89584c442270ebec41522c49
LeonidBabkin <Babkinleo@gmail.com>
2022-09-24 13:49:35 +0300

    great_div

--- brain_games/games/great_div.py
+++ brain_games/games/great_div.py
@@ -9,4 +9,6 @@ def calc_func():
     sequence = [i for i in range(1, max(a, b))]
     answer = max(list(filter(lambda x: a % x == 0 and b % x == 0, sequence)))
     question = f'Question: {a} {b}'
+    if a == b:
+        answer = a
     return question, str(answer)
